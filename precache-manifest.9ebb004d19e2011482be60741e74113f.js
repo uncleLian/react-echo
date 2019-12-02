ab07dd3fd0028c33b06ad3550bac9d54643b6dbd
uncleLian <771674109@qq.com>
2019-12-02 17:04:05 +0800

    create gh-pages

---
+++ precache-manifest.9ebb004d19e2011482be60741e74113f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ccebdff94ff5ae0def135b65015779",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d701a42e448a13f17b6f",
+    "url": "./static/js/2.ddedf4d6.chunk.js"
+  },
+  {
+    "revision": "eb39d53e0ab9c10d4b19",
+    "url": "./static/js/main.f4df58ba.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- src/assets/index.js
+++
@@ -1,2 +0,0 @@
-import '@/assets/css/reset.css'
-import '@/assets/css/global.styl'
